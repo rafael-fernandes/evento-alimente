4bf37ea9d50c281c47979dc6bcb2242b60aeab2f
Rafael Fernandes <spnrafael@gmail.com>
2018-03-11 13:08:22 -0300

    Atualiza responsividade da página inicial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  include ApplicationHelper
+
+  before_action :set_time
+
+  def set_time
+    @months, @days, @hours = calc_time
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def calc_time
+    seconds = Time.parse('2018-07-14 08:00:00 -0300') - Time.now
+    minutes = seconds / 60
+    hours = minutes / 60
+    days = hours / 24
+
+    months = (days / 30)
+    days = (months.modulo(1) * 30)
+    hours = (hours.modulo(1) * 24)
+
+    [months, days, hours].map!(&:to_i)
+  end
 end
